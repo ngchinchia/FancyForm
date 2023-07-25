12f54d450d179dcdc8c13f5205456f017da1315a
Ng Chin Chia <billyngchinchia@gmail.com>
2023-07-25 18:50:13 +0800

    add styling

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,48 +1,52 @@
-import React, { useState } from 'react';
-import './FormStyle.css'; // Make sure to have the CSS file in the same directory as this component
+import React, { useState } from "react";
+import "./FormStyle.css"; // Make sure to have the CSS file in the same directory as this component
 
 const Form = () => {
   // State to manage form input values
-  const [ethAddress, setEthAddress] = useState('');
-  const [amountToSend, setAmountToSend] = useState('');
-  const [otp, setOtp] = useState('');
+  const [ethAddress, setEthAddress] = useState("");
+  const [amountToSend, setAmountToSend] = useState("");
+  const [otp, setOtp] = useState("");
 
   // Event handler for form submission
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // Perform any desired actions on form submission, such as sending data to a server
-    console.log('Form submitted!');
+    console.log("Form submitted!");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="input-address">ETH Address</label>
-      <input
-        type="text"
-        id="input-address"
-        value={ethAddress}
-        onChange={(e) => setEthAddress(e.target.value)}
-      />
+    <div className="main-container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-container">
+          <label htmlFor="input-address">ETH Address</label>
+          <input
+            type="text"
+            id="input-address"
+            value={ethAddress}
+            onChange={(e) => setEthAddress(e.target.value)}
+          />
 
-      <label htmlFor="input-amount">Amount to send</label>
-      <input
-        type="text"
-        id="input-amount"
-        value={amountToSend}
-        onChange={(e) => setAmountToSend(e.target.value)}
-      />
+          <label htmlFor="input-amount">Amount to send</label>
+          <input
+            type="text"
+            id="input-amount"
+            value={amountToSend}
+            onChange={(e) => setAmountToSend(e.target.value)}
+          />
 
-      <label htmlFor="input-otp">OTP Authentication</label>
-      <input
-        type="number"
-        id="input-otp"
-        value={otp}
-        onChange={(e) => setOtp(e.target.value)}
-      />
+          <label htmlFor="input-otp">OTP Authentication</label>
+          <input
+            type="number"
+            id="input-otp"
+            value={otp}
+            onChange={(e) => setOtp(e.target.value)}
+          />
 
-      <button type="submit">SEND TOKENS</button>
-    </form>
+          <button type="submit">SEND TOKENS</button>
+        </div>
+      </form>
+    </div>
   );
 };
 
