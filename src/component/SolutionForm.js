9c112e854358d215f4f20eb6ab91fdd1f38e799f
Ng Chin Chia <billyngchinchia@gmail.com>
2023-07-30 00:44:59 +0800

    add styling

--- src/component/SolutionForm.js
+++ src/component/SolutionForm.js
@@ -1,66 +1,106 @@
-import React, {useState}from "react";
+import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
-import{ FormSchema } from "../schema/FormValidationSchema"
-import "./FormStyle.css"; 
+import { FormSchema } from "../schema/FormValidationSchema";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import "./FormStyle.css";
 
 const SolutionForm = () => {
-  const [isFocused, setIsFocused] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [valuesSubmitted, setValuesSubmit] = useState(null);
 
-  const handleEthFocus = (input) => {
-    setIsFocused(input)
-  }
+  // const data = JSON.stringify(valuesSubmitted)
 
   return (
     <div className="main-container">
-      <Formik
-        initialValues={{
-          ethAddress: "",
-          inputAmount: "",
-          otpAuthentication: "",
-        }}
-        validationSchema={FormSchema}
-        onSubmit={(values) => {
-          // same shape as initial values
-          console.log(values);
-        }}
-      >
-       {({ errors, touched }) => (
-      <Form>
-        <div className="form-container">
-          <div className="input-box">
-            <Field name="ethAddress" className="input-field"
-           />
-            <span className="place-holder">ETH Address</span>
-            {errors.ethAddress && touched.ethAddress ? (
-             <div className="error-eth">{errors.ethAddress}</div>
-           ) : null}
-          </div>
-
-          <div className="input-box">
-          <Field name="inputAmount" className="input-field" type="number" />
-            <span className="place-holder">Amount to send</span>
-            {errors.inputAmount && touched.inputAmount ? (
-             <div className="error-eth">{errors.inputAmount}</div>
-           ) : null}
-          </div>
-
-          <div className="input-box">
-          <Field name="otpAuthentication" className="input-field"/>
-            <span className="place-holder">OTP Authentication</span>
-            {errors.otpAuthentication && touched.otpAuthentication ? (
-             <div className="error-eth">{errors.otpAuthentication}</div>
-           ) : null}
-          </div>
-
-          <div className="input-box">
-            <button type="submit" className="send-token-button">
-              SEND TOKENS
-            </button>
-          </div>
+      {isLoading ? (
+        <div>
+          <ClimbingBoxLoader
+            color={"#00dfc4"}
+            size={30}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
         </div>
-        </Form>
-        )}
-      </Formik>
+      ) : isSubmitted ? (
+        <div className="values-submitted">
+          <h1 className="title-success">Transaction Completed!</h1>
+          <p>You sent to eth address: {valuesSubmitted.ethAddress} </p>
+          <p>Amount: {valuesSubmitted.inputAmount} ETH</p>
+        </div>
+      ) : (
+        <Formik
+          initialValues={{
+            ethAddress: "",
+            inputAmount: "",
+            otpAuthentication: "",
+          }}
+          validationSchema={FormSchema}
+          onSubmit={(values, { setSubmitting }) => {
+            setIsLoading(true);
+            setTimeout(() => {
+              setSubmitting(false);
+              setIsSubmitted(true);
+              setValuesSubmit(values);
+              setIsLoading(false);
+            }, 5000);
+          }}
+        >
+          {({ errors, touched, isSubmitting, handleSubmit }) => (
+            <Form onSubmit={handleSubmit}>
+              <div className="form-container">
+                <h1 className="app-title">Ethereum Mask</h1>
+                <div className="input-box">
+                  <Field
+                    name="ethAddress"
+                    className="input-field"
+                    placeholder=" "
+                  />
+                  <span className="place-holder">ETH Address</span>
+                  {errors.ethAddress && touched.ethAddress ? (
+                    <div className="error-eth">{errors.ethAddress}</div>
+                  ) : null}
+                </div>
+
+                <div className="input-box">
+                  <Field
+                    name="inputAmount"
+                    className="input-field"
+                    type="number"
+                    placeholder=" "
+                  />
+                  <span className="place-holder">Amount to send</span>
+                  {errors.inputAmount && touched.inputAmount ? (
+                    <div className="error-eth">{errors.inputAmount}</div>
+                  ) : null}
+                </div>
+
+                <div className="input-box">
+                  <Field
+                    name="otpAuthentication"
+                    className="input-field"
+                    placeholder=" "
+                  />
+                  <span className="place-holder">OTP Authentication</span>
+                  {errors.otpAuthentication && touched.otpAuthentication ? (
+                    <div className="error-eth">{errors.otpAuthentication}</div>
+                  ) : null}
+                </div>
+
+                <div className="input-box">
+                  <button
+                    type="submit"
+                    className="send-token-button"
+                    disabled={isSubmitting}
+                  >
+                    SEND TOKENS
+                  </button>
+                </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      )}
     </div>
   );
 };
--- src/schema/FormValidationSchema.js
+++ src/schema/FormValidationSchema.js
@@ -1,23 +1,17 @@
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
-const ethRegex = /^0x[a-fA-F0-9]{40}$/gm
+const ethRegex = /^0x[a-fA-F0-9]{40}$/gm;
+
 export const FormSchema = Yup.object().shape({
-    ethAddress: Yup.string()
-      .min(40, 'Eth address must be at least 40 characters in length!')
-      .max(42, 'Address shall not exceed 42 characters in length!')
-      .matches(ethRegex, 'Invalid address!')
-      .required('Required'),
-    inputAmount: Yup.number()
-      .positive('Amount must not be negative!')
-      .required('Required'),
-    otpAuthentication: Yup.string()
-      .matches(/^\d{6}$/, 'OTP must be 6 digits!')
-      .required('Required')
-  
-
-    // lastName: Yup.string()
-    //   .min(2, 'Too Short!')
-    //   .max(50, 'Too Long!')
-    //   .required('Required'),
-    // email: Yup.string().email('Invalid email').required('Required'),
-  });
+  ethAddress: Yup.string()
+    .min(40, "Eth address must be at least 40 characters in length!")
+    .max(42, "Address shall not exceed 42 characters in length!")
+    .matches(ethRegex, "Invalid address!")
+    .required("Required"),
+  inputAmount: Yup.number()
+    .positive("Amount must not be negative!")
+    .required("Required"),
+  otpAuthentication: Yup.string()
+    .matches(/^\d{6}$/, "OTP must be 6 digits!")
+    .required("Required"),
+});
